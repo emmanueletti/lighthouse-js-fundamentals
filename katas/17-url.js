921dc899a0ecab4b736f915b0d176cae845de942
Emmanuel Etti <emmanueletti@gmail.com>
2021-04-20 15:48:06 +0000

    update

--- katas/17-url.js
+++ katas/17-url.js
@@ -6,40 +6,64 @@ Key-value pairs are represented using an = character: key=value
 Multiple key-value pairs are separated using a & character: key1=value1&key2=value2
 */
 
-/* INITIAL THOUGHTS
-- create an empty object
-- read through the provided string
-- from begining to the first "=" should be saved as a variable and initialized into the empty object as a property
-- from that point to the next "&" should be saved into a variable and added to the object as a value of the first property
-- from an "&" to "=" should be another property
-- any any point, if a %20 is encountered then input a space
+/* INITIAL PLAN
+- read through string from begining to end
+- the first characters will be a string added to an empty object as a property name
+- when a '=' is seen, switch to creating a value
+- when a '&' is seen, add the value to the last property created and switch to creating another property name
 
 
 SUMMARY
 - properties = begining to "=", "&" to "="
 - values = "=" to "&"
 - space = " "
-
-if these can be turned into helper functions then
-
-- read through provided string
-- run properties function from beginning to "="
-- skip equal sign
-- run values function from "=" to "&"
-- skip "&"
-- run properties function from "&" to "="
 */
 
 //BUILD
 const urlDecode = function (text) {
-  let obj = {};
-
+  
+  //first clean text by adding an end marker, and converting '%20' to 'space'
+  let cleanText = '';
   for (let i = 0; i < text.length; i++) {
-    if (text[i] === '=') {
-      
+    if (text[i] === '%' || text[i] === '2') {
+      cleanText;
+    } else if (text [i] === '0') {
+      cleanText += ' ';
+    } else {
+      cleanText += text[i];
     }
   }
 
+  //then create a mode switcher that gets updated in the loop 
+  cleanText += '&';
+  let obj = {};
+  let mode = 'property';
+  let property = '';
+  let value = '';
+
+  for (let i = 0; i < cleanText.length; i++) {
+    
+    if (mode === 'property') {
+      if (cleanText[i] === '=') {
+        obj[property] = '';
+        mode = 'value';
+      } else {
+        property += cleanText[i];
+      }
+
+    } else if (mode === 'value') {
+      if (cleanText[i] === '&') {
+        obj[property] = value;
+        property = '';
+        value = '';
+        mode = 'property';
+      } else {
+        value += cleanText[i];
+      }
+    }
+
+  }
+  
   return obj;
 };
 
@@ -47,6 +71,6 @@ const urlDecode = function (text) {
 // TEST
 
 console.log(urlDecode("duck=rubber"));
-// console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
-// console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
-// console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
---
+++ katas/test.js
@@ -0,0 +1,6 @@
+for (let row = 0; row < 4; row++) {
+  for (let i = 0; i < 4; i++) {
+    console.log('###');
+  }
+  console.log('\n');
+}
